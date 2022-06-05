3611887fc407e2a11b328d925f297a0db3e4e691
Corvus <admin@github.com>
2022-06-05 15:48:09 +0800

    Add wip for day3 solution

---
+++ aoc/2021/day3.py
@@ -0,0 +1,46 @@
+def readData(filename):
+	data = []
+	f = open(filename, "r")
+	for line in f:
+		data.append(line.strip())
+
+	f.close()
+	return data
+
+
+def getNthDigit(binary, n):
+	return binary[n]
+
+
+def solvePart1(data):
+	numDigits = len(data[0])
+	gamma = ""
+	epsilon = ""
+
+	for i in range(numDigits):
+		nDigits = []
+
+		for binary in data:
+			nDigits.append(getNthDigit(binary, i))
+
+		gamma += str(max(nDigits))
+		epsilon += str(min(nDigits))
+
+		print(nDigits)
+		print(gamma)
+		print(epsilon)
+
+	# Convert to decimal
+	gamma = int(gamma, 2)
+	epsilon = int(epsilon, 2)
+
+	return gamma*epsilon
+
+
+if __name__ == "__main__":
+	filename = "testData.txt"
+	data = readData(filename)
+	print(f"Data: {data}")
+
+	part1Ans = solvePart1(data)
+	print(f"The answer for part 1 is: {part1Ans}")
