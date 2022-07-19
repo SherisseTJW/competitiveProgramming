c58e025e8f24654b55f841970f8561c861879b12
Corvus <dev@github.com>
2022-07-19 21:19:51 +0800

    feat: Solution for part1 and wip for part2

--- aoc/2021/day3.py
+++ aoc/2021/day3.py
@@ -1,7 +1,14 @@
 from ReadData import readData
 
+
 def getNthDigit(binary, n):
 	return binary[n]
+
+def filterData(data, i, type):
+	nDigits = []
+
+	for binary in data:
+		nDigits.append(getNthDigit(binary, i))
 
 
 def solvePart1(data):
@@ -15,12 +22,8 @@ def solvePart1(data):
 		for binary in data:
 			nDigits.append(getNthDigit(binary, i))
 
-		gamma += str(max(nDigits))
-		epsilon += str(min(nDigits))
-
-		print(nDigits)
-		print(gamma)
-		print(epsilon)
+		gamma += str(max(nDigits, key = nDigits.count))
+		epsilon += str(min(nDigits, key = nDigits.count))
 
 	# Convert to decimal
 	gamma = int(gamma, 2)
@@ -29,8 +32,28 @@ def solvePart1(data):
 	return gamma*epsilon
 
 
+def solvePart2(data):
+	numDigits = len(data[0])
+	o2_rating = ""
+	co2_rating = ""
+
+	o2_tempData = data
+	co2_tempData = data
+
+	for i in range(numDigits):
+		nDigits = []
+
+		for binary in o2_tempData:
+			nDigits.append(getNthDigit(binary, i))
+
+		o2_criteria = max(nDigits, key = nDigits.count)
+
+
+		co2_criteria = min(nDigits, key = nDigits.count)
+
+
 if __name__ == "__main__":
-	filename = "testData.txt"
+	filename = "data.txt"
 	data = readData(filename)
 	print(f"Data: {data}")
 
